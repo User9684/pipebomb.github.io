6d89d986ce10c74d93b159081e6e0ea79b64bef8
User <user9684official@gmail.com>
2021-12-22 11:39:23 -0600

    Create index.js

---
+++ error/index.js
@@ -0,0 +1,12 @@
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+var ec = getParameterByName('code');
+if(ec===404){
+  document.write('404 not fucking found!')
+} //man i have no damn clue how to do this LOL
