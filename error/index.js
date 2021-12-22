dd40f9d2e8fcd56422c92afef604673200fcd846
User <user9684official@gmail.com>
2021-12-22 11:44:06 -0600

    Update index.js

--- error/index.js
+++ error/index.js
@@ -1,12 +1,8 @@
-function getParameterByName(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 var ec = getParameterByName('code');
-if(ec===404){
+if(ec==='404'){
   document.write('404 not fucking found!')
 } //man i have no damn clue how to do this LOL
