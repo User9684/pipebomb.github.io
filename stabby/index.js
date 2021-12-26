d551200c84704a8758d751a5a8596b8734b681f3
User <user9684official@gmail.com>
2021-12-26 13:14:18 -0600

    Update index.js

--- stabby/index.js
+++ stabby/index.js
@@ -6,5 +6,5 @@ var person = getParameterByName('stabe');
 var stabCount = getParameterByName('count');
 if(person && stabCount){
   let string = `${person} got stabbed ${stabCount} times! Lmfao!!!!!`
-  document.write(`<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>pipebomb.shop</title>\n    <meta property=\"og:description\" content=\"${string}\">\n    <meta property=\"og:type\" content=\"website\">\n</head>\n<body>\n    <p>${string}</p>\n</body>\n</html>`)
+  document.write(`<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>pipebomb.shop</title>\n    	<meta property="og:site_name" content="stabby">\n     <meta property=\"og:description\" content=\"${string}\">\n    <meta property=\"og:type\" content=\"website\">\n</head>\n<body>\n    <p>${string}</p>\n</body>\n</html>`)
 }
