ef360a6a69634a4d85c7b34aa77c469d4ce99c48
User <user9684official@gmail.com>
2022-01-01 23:49:14 -0600

    Update index.js

--- truck/index.js
+++ truck/index.js
@@ -1,2 +1,6 @@
-setInterval(function(){
-alert('OMG THANKS FOR CLICKING ME!!!!')},1)
+alert('OMG THANKS FOR CLICKING ME!!!!');
+alert('OMFG THANKS FOR CLICKING OK!!! PLEASE CLICK AGAIN <3');
+alert(':O UR SO NICE!!!! I LIKE YOU!!! PLEASE CLICK AGAIN? ILL GIVE U A VERY COOL SURPRISE!!!');
+alert('໒(＾ᴥ＾)७');
+alert('wasn\'t that dog just like SOOOOOOOOOO adorbs?? like gurl, it was so cuuuteee!!!!!');
+alert('Oke byeeeee!!!!!')
