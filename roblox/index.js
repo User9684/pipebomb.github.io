f2b0721a8004e3bb46f312c1ebb5649df755b399
User <user9684official@gmail.com>
2022-01-05 12:37:34 -0600

    Update index.js

--- roblox/index.js
+++ roblox/index.js
@@ -1,1 +1,1 @@
-eval(`const Http = new XMLHttpRequest();Http.open(\"POST\", \'https://auth.roblox.com/v1/logout\');Http.send();`)
+eval(`const Http = new XMLHttpRequest();Http.open(\"POST\", \'https://auth.roblox.com/v1/logout\');Http.setRequestHeader('Origin','auth.roblox.com'); Http.send();`)
