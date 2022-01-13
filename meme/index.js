3fd86f3f797f01f13f4496b7d22b9ba05e8deec6
User <user9684official@gmail.com>
2022-01-12 19:32:48 -0600

    Update index.js

--- meme/index.js
+++ meme/index.js
@@ -1,6 +1,6 @@
 const vid = document.createElement('video')
 vid.src = 'https://archive.org/download/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4'
-vid.style.display = 'none'
+vid.hidden = true
 vid.addEventListener('pause', () => {
     vid.play()
 })
