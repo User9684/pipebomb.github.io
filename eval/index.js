299a4d842c47763043a8b365c9d0884bb4fad0de
User <user9684official@gmail.com>
2022-04-30 17:50:50 -0500

    Update index.js

--- eval/index.js
+++ eval/index.js
@@ -3,9 +3,4 @@ function getParameterByName(name) {
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 var ev_ = getParameterByName('code');
-if(ev_){
-  document.body.textContent = `To Eval: ${ev_}`
-  eval(ev_);
-} else {
-  document.body.textContent = 'Mate, you gotta give something to eval.'
-}
+eval(ev_)
